544e553a34867499d0241f95fff855a2736af006
sw4q3ar2 <biggercsgo@gmail.com>
2023-09-12 12:19:34 +0200

    Factory feltoltes

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,20 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create([
+            'id' => 1,
+            'name' => 'No Way',
+            'email' => 'futakenyer@mail.qu',
+            'password' => 'Aa123456@',
+        ]);
+
+        User::create([
+            'id' => 2,
+            'name' => 'bara',
+            'email' => 'baracsds@mail.qu',
+            'password' => 'Aa123456@',
+        ]);
     }
 
     /**
